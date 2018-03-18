eb3692fc9a8fd5808c010cb0c4d5da82bc83d69f
Ramprakash Ram Mohan <rammohan@juniper.net>
2018-03-17 21:40:35 -0700

    Derive parameters for kolla and contrail provisioning
    * Derive the following parameters automatically if possible. (User provided
            entries will take precedence):
        1. kolla_internal_vip_address for AIO
        2. contrail_interface_address
        4. network_interface
        5. kolla_external_vip_interface
        6. kolla_external_vip_address for AIO
        7. enable_haproxy set to "no" for AIO
        8. KEYSTONE_AUTH_HOST
        9. PHYSICAL_INTERFACE
    
    * include logic to derive ctrl-data-ip to populate /etc/hosts when
      CTRL_DATA_NETWORK is specified in contrail_configuration
    
    * Make the default password be contrail123 to be in sync with the contrail
      defaults
    
    Change-Id: Idc1143a4e235842849ba2e2134004809c5bd7ed5
    Closes-Bug: #1755590

--- playbooks/filter_plugins/ctrl_data_intf_dict.py
+++ playbooks/filter_plugins/ctrl_data_intf_dict.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 from ansible.errors import AnsibleFilterError
+import ipaddress
 
 class FilterModule(object):
     def filters(self):
@@ -8,7 +9,18 @@ class FilterModule(object):
             'ctrl_data_intf_dict': self.ctrl_data_intf_dict
         }
 
-    def ctrl_data_intf_dict(self, instances, contrail_config, kolla_config):
+    @staticmethod
+    def get_host_ctrl_data_nw_if(my_ip, my_vars, cidr):
+        ctrl_data_nw = ipaddress.ip_network(cidr)
+        for iface in my_vars['ansible_interfaces']:
+            if_str = 'ansible_' + iface
+            if_ipv4 = my_vars.get(if_str).get('ipv4', None)
+            if if_ipv4 and ipaddress.ip_address(if_ipv4['network']) == ctrl_data_nw.network_address:
+                return iface
+        return None
+
+    def ctrl_data_intf_dict(self, instances, contrail_config,
+                            kolla_config, hostvars):
         host_intf = {}
         kolla_globals = kolla_config.get('kolla_globals', {})
         for k,v in instances.iteritems():
@@ -16,6 +28,12 @@ class FilterModule(object):
                     kolla_globals.get('network_interface', None))
             if tmp_intf != None:
                 host_intf[v['ip']] = tmp_intf
+
+            if contrail_config.get('CTRL_DATA_NETWORK'):
+                tmp_intf = FilterModule.get_host_ctrl_data_nw_if(v['ip'], hostvars[v['ip']],
+                        contrail_config.get('CTRL_DATA_NETWORK'))
+                if tmp_intf != None:
+                    host_intf[v['ip']] = tmp_intf
 
             for i,j in v.get('roles', {}).iteritems():
                 if j is not None:
