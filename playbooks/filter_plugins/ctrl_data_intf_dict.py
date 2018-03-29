ad67454f8057682f64f7d552dcb79ea41bb36405
Ramprakash Ram Mohan <rammohan@juniper.net>
2018-03-28 18:28:18 -0700

    Fix deriving network_interface based on CONTROL_DATA_NETLIST
    Side effect of bug #1755590
    The network_interface_detected variable should be per-host and cannot be global.
    Similarly for external_interface_detected
    
    PATCHSET 4: Accomodate the case where "roles" is not defined at all for an
    instance defined in instances.yaml
    
    PATCHSET 5: When it is not possible to detect network_interface, use the
    management network interface
    
    Change-Id: I4939611187c39fa216e455ebd35441d5b789be7b
    Closes-Bug: #1759731

--- playbooks/filter_plugins/ctrl_data_intf_dict.py
+++ playbooks/filter_plugins/ctrl_data_intf_dict.py
@@ -6,7 +6,8 @@ import ipaddress
 class FilterModule(object):
     def filters(self):
         return {
-            'ctrl_data_intf_dict': self.ctrl_data_intf_dict
+            'ctrl_data_intf_dict': self.ctrl_data_intf_dict,
+            'mgmt_intf_dict': self.mgmt_intf_dict
         }
 
     @staticmethod
@@ -51,6 +52,29 @@ class FilterModule(object):
             for i,j in v.get('roles', {}).iteritems():
                 if j is not None:
                     tmp_intf = j.get('network_interface', None)
+                    if tmp_intf != None:
+                        host_intf[v['ip']] = tmp_intf
+
+        return host_intf
+
+    def mgmt_intf_dict(self, instances, contrail_config,
+                            kolla_config, hostvars):
+        host_intf = {}
+        kolla_globals = kolla_config.get('kolla_globals', {})
+        for k,v in instances.iteritems():
+            for i in hostvars[v['ip']].get('ansible_interfaces', None):
+                if_str = 'ansible_' + i
+                if_ipv4 = hostvars[v['ip']].get(if_str).get('ipv4', None)
+                if if_ipv4 and if_ipv4.get('address', None) == v['ip']:
+                    host_intf[v['ip']] = i
+
+            tmp_intf = kolla_globals.get('kolla_external_vip_interface', None)
+            if tmp_intf != None:
+                host_intf[v['ip']] = tmp_intf
+
+            for i,j in v.get('roles', {}).iteritems():
+                if j is not None:
+                    tmp_intf = j.get('kolla_external_vip_interface', None)
                     if tmp_intf != None:
                         host_intf[v['ip']] = tmp_intf
 
